e9727900e9fa53bb2c2d2f5abbf6899685da9c5c
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2025-11-11 03:23:38 +0000

    Add encode and decode methods in Solution class
    Implement encoding and decoding methods for strings.

---
+++ easy/encode_and_decode_string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        self.q = []
+        self.s = []
+        self.s1 = ""
+        for i,n in enumerate(strs):
+            print(len(n))
+            self.q.append(len(n) + (self.q[-1] if self.q else 0)) # only adding last one unlike sum(self.q)
+            self.s1 = self.s1 + n
+        return self.s1
+
+    def decode(self, s: str) -> List[str]:
+        for i in range(len(self.q)):
+            self.s.append(self.s1[0 if i==0 else self.q[i-1]: self.q[i]])
+        return self.s
