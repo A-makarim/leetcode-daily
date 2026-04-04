9f5c86a37df08b711b029a0ab98c764275471d94
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-04-04 16:19:23 +0100

    Create Diameter of Binary Tree.py

---
+++ easy/Diameter of Binary Tree.py
@@ -0,0 +1,33 @@
+"""
+KEY INSIGHTS
+SAME MAX DEPTH ALGORITHM
+JUST NOW STORING THE MAX DFS OF LEFT AND RIGHT TO A GLOBAL VARIABLE
+DFS AUTO GOES TO EVERY NODE
+WE ONLY NEED TO STORE THE ADDITION OF LEFT+RIGHT IN SELF.DIAM
+"""
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        # use max depth but include left max and right max both
+
+        self.diam = 0
+        
+        def dfs(root):
+            if root == None:
+                return 0
+            self.diam = max(self.diam, dfs(root.left) + dfs(root.right))
+            return 1 + max(dfs(root.left), dfs(root.right))
+
+        dfs(root)
+        return self.diam
+        
